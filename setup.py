3e1706dc54373f335185d74127a830d14b672704
shitalsupekar <shitalsupekar138@gmail.com>
2023-06-27 08:10:53 +0530

    EDA and Problem Statement

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
-
-from setuptools import setup, find_packages
-
-
+from setuptools import find_packages,setup
 from typing import List
 
-def get_requirements(file_path: str) -> List[str]:
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
     '''
-    This function will return the list of requirements
+    this function will return the list of requirements
     '''
-    requirements = []
+    requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-        if "HYPHEN_E_DOT" in requirements:
-            requirements.remove("HYPHEN_E_DOT")
-
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
 setup(
-    name='MlProject',
-    version='0.0.1',
-    author='Shital',
-    author_email='shitalsupekar138@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
 )
